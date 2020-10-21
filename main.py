216e41fedf9dba2942ae68eff054065177650cb0
ssravjee <ssravjee@users.noreply.github.com>
2020-10-21 13:48:16 +0100

    Simple program written in Python that reads files out-loud.

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2 as p2
+file = open('C:/pdf/lipsum.pdf','rb')
+book = p2.PdfFileReader(file)
+page = book.getPage(0)
+text = page.extractText()
+print (text)
+speaker = pyttsx3.init()
+speaker.say(text)
+speaker.runAndWait()
+print('Program completed.')
+
