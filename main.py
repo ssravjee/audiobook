b0b9ac44c013479493ec271da087655b7b16d6c0
ssravjee <ssravjee@users.noreply.github.com>
2020-10-21 14:38:17 +0100

    added comments for ease of understading the program flow. :-)

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pyttsx3
 import PyPDF2 as p2
+# Get file handle, you want to read-aloud
 file = open('./pdf/lipsum.pdf','rb')
+# Read contents of file into book object
 book = p2.PdfFileReader(file)
+# Load first page (page 0 i.e. page 1) contents into page object
 page = book.getPage(0)
+# Store contents of page in a text object
 text = page.extractText()
+# Print the text for debugging purpose.
+# Sometimes the pyttsx3 package is not able to read all types of pdf files
+# You may comment it by addeding the # before the line below, it's your choice
 print (text)
+# Initialize the read-aloud python package
 speaker = pyttsx3.init()
+# Read aloud the pdf page selected
 speaker.say(text)
+# The line below is used
 speaker.runAndWait()
 print('Program completed.')
-
